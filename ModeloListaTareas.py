c5210cb54dea037c651b2b70b047c76a1e36ccc4
Cristian <cregueiromartinez@danielcastelao.org>
2024-11-19 12:30:38 +0100

    tick añadido en tarea completada

--- ModeloListaTareas.py
+++ ModeloListaTareas.py
@@ -2,9 +2,11 @@ import sys
 from cProfile import label
 
 from PyQt6.QtCore import Qt, QAbstractListModel
-from PyQt6.QtGui import QPalette, QColor
+from PyQt6.QtGui import QPalette, QColor, QImage
 from PyQt6.QtWidgets import (QMainWindow, QApplication, QVBoxLayout, QPushButton, QWidget, QLabel, QLineEdit,
                              QGridLayout)
+
+tick = QImage("tick.png")
 class ModeloTareas (QAbstractListModel):
     def __init__(self, tarefas = None):
         super().__init__()
@@ -14,6 +16,11 @@ class ModeloTareas (QAbstractListModel):
         if role == Qt.ItemDataRole.DisplayRole:
             _, texto = self.tarefas[index.row()]
             return texto
+
+        if role == Qt.ItemDataRole.DecorationRole:
+            estado,_ = self.tarefas[index.row()]
+            if estado:
+                return tick
 
     def rowCount(self, index):
         return len(self.tarefas)
